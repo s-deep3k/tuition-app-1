df32296bc8b29a6c4e910ad003da3a37c481a0aa
s-deep3k <48375087+s-deep3k@users.noreply.github.com>
2020-12-09 16:11:50 +0530

    Completed imagelinks.js
    sob changa hu

--- imagelinks/imageLinks.js
+++ imagelinks/imageLinks.js
@@ -1,7 +1,35 @@
 export const CoursesImgs = [
-    `public/images/1_B_Sc_BOTANY.png`,  //First array elements name is B.Sc Botany
-    `public/ima....`                    //Each and every images path should start from '/' and then whatever the path in the public folder. 
-] 
+    `/courses-img/1_B_Sc_BOTANY.png`,
+    `/courses-img/2_B.Sc_Zoology.png`,
+    `/courses-img/3_B.Sc_Chemistry.png`,
+    `/courses-img/4_M.Sc Entrance Examination.png`,
+    `/courses-img/5_M.Sc Regular.png`,
+    `/courses-img/6_M.Sc Distance.png`,
+    `/courses-img/7_JOINT CSIR-UGC NET Examination.png`,
+    `/courses-img/8_IIT-GATE Examination.png`,
+    `/courses-img/9_IIT-JAM Examination.png`,
+    `/courses-img/10_WBSET_1.png`,
+    `/tutors-img/01_Jit.JPG`,
+    `/tutors-img/02_Arindam.jpg`,
+    `/tutors-img/03_Sagarika (2).JPG`,
+    `/tutors-img/04_Sandipta.jpg`,
+    `/tutors-img/06_Rabishankar.jpg`,
+    `/tutors-img/07_Debanjan.jpg`,
+    `/tutors-img/08_Sudeshna.jpg`,
+    `/tutors-img/09_Anurima.JPG`,
+    `/tutors-img/10_Jayita.jpg`,
+    `/tutors-img/11_Arpan.jpg`,
+    `/tutors-img/12_Anirban.jpg`,
+    `/tutors-img/13_Manisha.jpg`,
+    `/tutors-img/14_Susmita.jpg`,
+    `/tutors-img/15_Soham.jpg`,
+    `/tutors-img/16_Entaj.jpg`,
+    `/tutors-img/17_Archita Photo.jpg`,
+    `/tutors-img/18_Brahmarshi photo.jpg`,
+    `/tutors-img/Prithu.jpg`,
+    `/tutors-img/Sanak.jpg`,
+    `/tutors-img/Sandip.jpg`
+    ]
 
 
 // create links according the api array id. 
